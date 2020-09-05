878297d4eaedfb434eac17a04caa76fefd73c3a0
gimpyspetsnaz <shrddwt@gmail.com>
2020-09-05 11:59:59 -0500

    Split apart function components and their children components into separate js files. I can breathe now! TODO: going to fix the random question method so that it never returns null, and reproduce that bug so I better understand it. Next step is to have the ability for the user to add their own custom flash card belonging to any category

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import * as Questions from './StaticCards/Questions.js';
-import Checkbox from './Checkbox.js';
 import { Navbar, Nav, NavDropdown, Button, Form, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function ToggleAnswer(props) {
-  return (
-    <button className='greybutton'
-      onClick={props.onClick}>
-      {props.isVisible ? 'Hide answer' : 'Show answer'}
-    </button>
-  )
-}
+import FlashCardApp from './mini-apps/FlashCardApp.js'
 
-function NextQuestion(props) {
-  return (
-    <>
-      <button
-        className='greybutton'
-        onClick={props.onClick}>
-        Next question
-      </button>
-    </>
-  )
-}
-
-
-
-function Card(props) {
-  return (
-    <>
-      <div className='question'>{props.question}</div>
-      <div className='answer'
-        hidden={!props.answerVisible}>{props.answer}</div>
-      <ToggleAnswer
-        onClick={props.onClick}
-        isVisible={props.answerVisible}
-      />
-    </>
-  )
-}
-
-const CATEGORIES = ['csharp', 'html', 'css', 'sql'];
 const PAGES = ['Home', 'Flash cards', 'Multiple choice', 'Recruiter tracking'];
 
 function App() {
@@ -62,7 +24,7 @@ function App() {
 const renderNavBar = (setCurrentPage) => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Full Stack Trainer</Navbar.Brand>
+      <Navbar.Brand onClick={() => setCurrentPage(PAGES[0])}>Full Stack Trainer</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
@@ -94,144 +56,6 @@ const renderCurrentPage = (currentPage) => {
     default:
       return Home();
   }
-}
-
-function FlashCardApp() {
-
-  const getLocalStorageCategories = () => {
-    const rawCategories = JSON.parse(localStorage.getItem('categories'));
-    return rawCategories?? [];      
-  };
-
-  const saveLocalStorageCategories = (cats) => {
-    localStorage.setItem('categories', JSON.stringify(cats));
-  };
-
-  const [answerVisible, setAnswerVisible] = useState(false);
-  const [answer, setAnswer] = useState("");
-  const [question, setQuestion] = useState("Click 'next question' for question");
-  const [categories, setCategories] = useState(CATEGORIES);
-  const [userCategories, setUserCategories] = useState(getLocalStorageCategories())
-  const [checkedCategories, setCheckedCategories] = useState([]);
-  
-
-  const [addCategoryText, setAddCategoryText] = useState();
-  const [isAddingCategory, setIsAddingCategory] = useState(false);
-
-
-  
-  
-
- 
-
-  const handleToggleAnswerClick = () => {
-    setAnswerVisible(!answerVisible);
-  }
-
-  const handleNextCardClick = (setQuestion, setAnswer) => {
-    if (checkedCategories.length > 0) {
-      let card = Questions.getRandom(checkedCategories);
-      setQuestion(card.question);
-      setAnswer(card.answer);
-      setAnswerVisible(false);
-    }
-    else {
-      window.alert("Please check at least one category in order to display next question.")
-    }
-  }
-
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    if (checked) {
-      setCheckedCategories([...checkedCategories, name]);
-    } else {
-      setCheckedCategories(checkedCategories.filter(category => category !== name));
-    }
-  };
-
-  const handleAddCategoryNameChange = (event) => {
-    setAddCategoryText(event.target.value);
-  };
-
-  const handleNewCategoryClick = () => {    
-    setIsAddingCategory(!isAddingCategory);
-  }
-
-  useEffect(() => {
-    saveLocalStorageCategories(userCategories);
-  }, [userCategories]); // run whenever beerCount is changed
-
-  const handleConfirmCategory = () =>{
-    if([...categories,...userCategories].includes(addCategoryText)){
-      window.alert(`Category ${addCategoryText} already exists!`);
-    }
-    else{
-      setUserCategories([...userCategories,addCategoryText]);
-      setIsAddingCategory(!isAddingCategory);
-    }    
-  }
- 
-  return (
-    
-    <>
-      <Card
-        question={question}
-        answer={answer}
-        answerVisible={answerVisible}
-        onClick={() => handleToggleAnswerClick()}
-      />
-      <NextQuestion
-        onClick={() => handleNextCardClick(setQuestion, setAnswer)}
-      />
-      <div className='categoryContainer'>
-        {categories.map(category => {
-          return (<Checkbox
-            label={category}
-            isChecked={checkedCategories.includes(category)}
-            onCheckboxChange={handleCheckboxChange}
-            key={category}
-          />)
-        })}       
-        {userCategories.map(category => {
-          return (<Checkbox
-            label={category}
-            isChecked={checkedCategories.includes(category)}
-            onCheckboxChange={handleCheckboxChange}
-            key={category}
-          />)
-        })}       
-      </div>
-      <div className='addCategory'>
-<AddCategory
-handleConfirmCategory={handleConfirmCategory}
-handleAddCategoryNameChange={handleAddCategoryNameChange}
-handleNewCategoryClick={handleNewCategoryClick}
-isAddingCategory={isAddingCategory}
-/>
-      </div>
-    </>
-  )
-}
-
-function AddCategory(props) {
-  return (
-    <>
-      <img
-        onClick={props.handleNewCategoryClick}
-        alt="missing"
-        width='12px'
-        height='12px'
-        src={props.isAddingCategory ? '/images/cancel.png' : '/images/add.png'}
-        margin-right='3px' />
-        {!props.isAddingCategory? <span>Add category</span> 
-        : <>
-        <input type='text' value={props.addCategoryText} onChange={props.handleAddCategoryNameChange}>
-          </input>
-          <button className='confirmCategory' onClick={props.handleConfirmCategory}>Confirm category
-          </button></> 
-          }        
-    </>
-  )
 }
 
 function Home() {
