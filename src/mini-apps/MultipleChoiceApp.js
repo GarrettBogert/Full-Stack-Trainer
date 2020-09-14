a0921b7e9c44949a4a9c2b150ea815aa8533af2b
gimpyspetsnaz <shrddwt@gmail.com>
2020-09-14 04:20:01 -0500

    the word 'below' for the layperson

--- src/mini-apps/MultipleChoiceApp.js
+++ src/mini-apps/MultipleChoiceApp.js
@@ -222,7 +222,7 @@ export default function MultipleChoiceApp(props) {
             <h1 className='miniapptitle'>Multiple choice quizzes</h1>
             {checkedCategories.length === 0 ?
                 <>
-                    <div>To get started, select one or many categories and click 'Start Quiz' to begin a quiz session</div>
+                    <div>To get started, select one or many categories below and click 'Start Quiz' to begin a quiz session</div>
                 </>
                 : null
             }
